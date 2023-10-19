f88c68df5138657db1b37831681613ffd7a8c7b2
Logan Van Meter <vanmeterlj@gmail.com>
2023-10-19 12:59:54 -0400

    Added Chuck Norris fact generator

--- src/scripts/Nutshell.js
+++ src/scripts/Nutshell.js
@@ -1,7 +1,8 @@
 import { LogOutButton } from "./auth/LogoutButton.js";
 import { Chat } from "./Chat.js";
+import { ChuckNorrisFacts } from "./ChuckNorris.js";
 
 export const Nutshell = () => {
 	return `${LogOutButton()}
-	${Chat()}`;
+	${Chat()}${ChuckNorrisFacts()}`;
 };
